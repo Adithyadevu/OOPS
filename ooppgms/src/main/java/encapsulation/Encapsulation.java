f5e0aff7ee206c8d4ae89c5bcb5fb276c67122a4
acer <acer@DEVU>
2024-04-23 21:55:35 +0530

    Encapsulation oops concept

---
+++ ooppgms/src/main/java/encapsulation/Encapsulation.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class Encapsulation {
+	private String name;
+	public void getname() {
+		System.out.println(name);
+	}
+public void setname(String name) {
+		this.name=name;
+	}
+}
+
