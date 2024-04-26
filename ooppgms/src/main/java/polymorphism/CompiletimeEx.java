613a8979151f7a46a0fed679fa435f2baec03173
acer <acer@DEVU>
2024-04-26 23:54:00 +0530

    Compiletime polymorphism

---
+++ ooppgms/src/main/java/polymorphism/CompiletimeEx.java
@@ -0,0 +1,33 @@
+package polymorphism;
+
+public class CompiletimeEx {
+	public void disp(String a) {
+		System.out.println(a);
+	}
+	public void disp() {
+		System.out.println("This is Method Overloading");
+	}
+	public int disp(int a,int b) {
+		return a+b;
+}
+	public void disp(int a) {
+		System.out.println(a);
+	}
+	public char disp(char a) {
+		return a;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CompiletimeEx obj=new CompiletimeEx();
+		obj.disp();
+		obj.disp("Compile time polymorphism");
+		int x=obj.disp(5,10);
+		System.out.println(x);
+		obj.disp(100);
+		char y=obj.disp('M');
+		System.out.println(y);
+
+	}
+
+}
