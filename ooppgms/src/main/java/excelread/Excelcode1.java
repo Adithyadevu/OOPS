2994ab3a6d7b5393578014d3f7b583d5c0d50cee
acer <acer@DEVU>
2024-05-07 22:01:35 +0530

    Excelread

---
+++ ooppgms/src/main/java/excelread/Excelcode1.java
@@ -0,0 +1,16 @@
+package excelread;
+
+import java.io.IOException;
+
+public class Excelcode1 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+String y=Exxelcode.getStringData(3,0);
+System.out.println(y);
+String m=Exxelcode.getIntegerData(4,1);
+System.out.println(m);
+
+	}
+
+}
